4e7e0564f8e8b0fb20d7f43d5b0740e4358c0c0b
J-Okoto <okotojr@gmail.com>
2020-10-11 14:10:18 +0300

    added image model and did third migrations to update db

--- gallery_app/models.py
+++ gallery_app/models.py
@@ -38,4 +38,55 @@ class Category(models.Model):
     
     def delete_category(self):
         self.delete()
+
+class Image(models.Model):
+    name = models.CharField(max_length =80)
+    description = models.TextField()
+    location = models.ForeignKey(Location, related_name='location', on_delete=models.DO_NOTHING)
+    category = models.ForeignKey(Category, related_name='category', on_delete=models.DO_NOTHING)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    
+    
+    class Meta:
+        ordering = ["pub_date"]
+        verbose_name = "Image"
+        verbose_name_plural = "Images"
+    
+    def summary(self):
+        return self.description[:100]+" ......"
+    
+    
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
+    
+    def save_image(self):
+        self.save()
+    
+    
+    def delete_image(self):
+        self.delete()
+    
+    def update_image(self):
+        pass
+        
+
+    def get_image_by_id(cls):
+        images = cls.objects.get(pk=id)
+        return images
+    
+    def search_image(category):
+        pass
+    
+    def filter_by_location(location):
+        pass
+    
+    
+    @classmethod
+    def search_by_category(cls,search_term):
+        searched_images = cls.objects.filter(category__icontains = search_term)
+        return searched_images
+    
+ 
+    
+    
     
