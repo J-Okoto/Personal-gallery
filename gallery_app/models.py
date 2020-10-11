ef494a4be0db860596f01b91218712405d914cd3
J-Okoto <okotojr@gmail.com>
2020-10-11 13:44:16 +0300

    added location model

--- gallery_app/models.py
+++ gallery_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Location(models.Model):
+    name = models.CharField(max_length =80, unique='True')
+    
+    class Meta:
+        ordering = ["name"]
+        verbose_name = "Location"
+        verbose_name_plural = "Locations"
+        
+        
+    def __str__(self):
+        return self.name
+    
+    def save_location(self):
+        self.save()
+        
+    
+    def delete_location(self):
+        self.delete()
