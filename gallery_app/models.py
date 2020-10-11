46e6ae8310ceb011bae5cf0cec7fe30fba973ca6
J-Okoto <okotojr@gmail.com>
2020-10-11 14:00:37 +0300

    added categories model and did second migration to update the database

--- gallery_app/models.py
+++ gallery_app/models.py
@@ -20,3 +20,22 @@ class Location(models.Model):
     
     def delete_location(self):
         self.delete()
+
+class Category(models.Model):
+    name = models.CharField(max_length =80, unique='True')
+    
+    class Meta:
+        ordering = ["name"]
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+    
+    def __str__(self):
+        return self.name
+    
+    
+    def save_category(self):
+            self.save()
+    
+    def delete_category(self):
+        self.delete()
+    
