b776da3288c38823786d6fdcea67ec099705539d
J-Okoto <okotojr@gmail.com>
2020-10-11 11:27:35 +0300

    created views function to return index page

--- gallery_app/views.py
+++ gallery_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    '''
+    Index function loads the start up page 
+    '''
+    
+    return render(request,'index.html')
+
--- personal_gallery/settings.py
+++ personal_gallery/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'gallery_app.apps.GalleryAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
