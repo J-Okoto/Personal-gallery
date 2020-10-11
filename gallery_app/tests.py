be2dc47168df3ef58b41bf800e5f508f21ba58b5
J-Okoto <okotojr@gmail.com>
2020-10-11 15:12:55 +0300

    added tests to test location and category

--- gallery_app/tests.py
+++ gallery_app/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
-
+from .models import Image, Location, Category
+import datetime as dt
 # Create your tests here.
+
+class LocationTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.loc1= Location(name = 'Nairobi')
+        
+# Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.loc1,Location))
+
+
+class CategoryTestClass(TestCase):
+    def setUp(self):
+        self.cat1 = Category(name = 'wedding')
+
