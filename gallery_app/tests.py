396c68759e5c2ea10e04c6fb0ba77e59a87d9ebf
J-Okoto <okotojr@gmail.com>
2020-10-12 22:49:43 +0300

    addded test to save and delete images

--- gallery_app/tests.py
+++ gallery_app/tests.py
@@ -17,12 +17,30 @@ class CategoryTestClass(TestCase):
     def setUp(self):
         self.cat1 = Category(name = 'wedding')
 
+    
+
 class ImageTestClass(TestCase):
     def setUp(self):
-        # Creating a new editor and saving it
+        # Creating a new image and saving it
         self.image1= Location(name = 'India')
         self.image1.save_location()
+
+        self.category = Category(name='portrait')
+        self.category.save_category()
         
-        self.new_image= Image(name = 'oldman',description = 'This is a random post',location = self.image1)
-        self.new_image.save()
+        self.new_image= Image(name = 'oldman',description='this is a test image', location=self.location,category=self.category)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image_test, Image))
+
+
+    def test_save_image(self):
+        self.image_test.save_image()
+        after = Image.objects.all()
+        self.assertTrue(len(after) > 0)
+
+    def test_delete_image(self):
+        self.image_test.delete_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) == 0)
 
