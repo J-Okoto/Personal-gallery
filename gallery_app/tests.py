e60cfbaf50faa5bacafe996bcb3f9c316b5dfaf4
J-Okoto <okotojr@gmail.com>
2020-10-12 23:31:34 +0300

    added test to delete and add locations

--- gallery_app/tests.py
+++ gallery_app/tests.py
@@ -6,16 +6,41 @@ import datetime as dt
 class LocationTestClass(TestCase):
     # Set up method
     def setUp(self):
-        self.loc1= Location(name = 'Nairobi')
+        self.location= Location(name = 'Nairobi')
         
 # Testing  instance
     def test_instance(self):
-        self.assertTrue(isinstance(self.loc1,Location))
+        self.assertTrue(isinstance(self.location,Location))
+
+    def test_save_location(self):
+        self.location.save_location()
+        locations = Location.get_locations()
+        self.assertTrue(len(locations) > 0)
+
+    def test_delete_location(self):
+        self.location.delete_location()
+        location = Location.objects.all()
+        self.assertTrue(len(location) == 0)
+
+
 
 
 class CategoryTestClass(TestCase):
     def setUp(self):
-        self.cat1 = Category(name = 'wedding')
+        self.cat1 = Category(name = 'portrait')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.cat1, Category))    
+
+    def test_save_category(self):
+        self.cat1.save_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories) > 0)
+
+    def test_delete_category(self):
+        self.cat1.delete_category()
+        category = Category.objects.all()
+        self.assertTrue(len(category) == 0)  
 
     
 
